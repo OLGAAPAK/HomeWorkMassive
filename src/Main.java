cfc9b5e916f732f1ec3c48a6f9a255d5b66602b1
OLGAAPAK <olga.pak777@yandex.ru>
2023-11-18 16:44:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr2 = new int[15];
+
+        for (int i = 0; i < arr2.length; i++) {
+            if (i < 2) {
+                arr2[i] = i;
+            }
+            else {
+                arr2[i] = arr2[i-1] + arr2[i-2];
+            }
+            if (arr2[i] % 2 == 0) {
+                System.out.println(arr2[i]);
+            }
+        }
+
+    }
+}
